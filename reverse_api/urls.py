4d257283bf66b1d90aa7e75eab39e37522600d0d
julioquintanazaez <jupyterdevs2022@gmail.com>
2025-01-03 00:08:03 -0500

    Test deploy

--- reverse_api/urls.py
+++ reverse_api/urls.py
@@ -2,7 +2,6 @@ from django.urls import include, path
 from rest_framework import routers
 from .views.exceluploadview import ExcelUploadView
 
-from .views.homeview import HomeView
 from .views.genesview import GenesListView, GenesAllelesListView
 from .views.allelesview import AllelesListView
 from .views.allelescombinationsviews import AllelesCombinationsByIDListView, AllelesCombinationsAllListView
